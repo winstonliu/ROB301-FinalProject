9b04c93817d43989a104fdb5a6d222234586dfce
winstonliu <winstonl95@hotmail.com>
2015-11-17 14:54:15 -0500

    Added VFH+ calculations.

--- src/pathplanner.java
+++ src/pathplanner.java
@@ -1,9 +1,14 @@
+import java.awt.List;
 import java.awt.Point;
+import java.io.FilterInputStream;
+import java.util.ArrayList;
 
 import javax.print.attribute.standard.Destination;
+import javax.security.auth.kerberos.KerberosTicket;
 
 import org.freedesktop.DBus.Description;
 import org.omg.CORBA.PUBLIC_MEMBER;
+import org.omg.IOP.CodeSets;
 
 import lejos.robotics.navigation.Pose;
 
@@ -22,23 +27,45 @@ public class pathplanner {
 
 	public lejos.robotics.geometry.Point goal;
 	public Pose myPose;
+	static Pose lastPose;
 	public Boolean finalApproach;
+	public lejos.robotics.geometry.Point toGoal;
+	public float projDist; // dt
+	
+	// Coefficients
+	public float angRes; // angular resolution of histogram
+	public float FULLROT = 360; // depends on if degrees or radians
+	// weight values for cost function
+	public int m1; // mu1
+	public int m2; // mu2
+	public int m3; // mu3
 	
 	public pathplanner(Pose currentPose, lejos.robotics.geometry.Point destination) {
 		// TODO Auto-generated constructor stub
 		myPose = currentPose;
 		goal = destination;
+		toGoal = goal.subtractWith(myPose.getLocation());
 		finalApproach = Boolean.FALSE;
 	}
-	
-	public lejos.robotics.geometry.Point straightPathOrientation () {
-		return goal.subtractWith(myPose.getLocation());
-	}
-	
-	public void planPath() {
-		lejos.robotics.geometry.Point straightPath = straightPathOrientation();
-		
-		// Find closest candidate valley, gradient descent?
-	}
 
+	public float getPCDCost(float c0) {
+        // Use VFH*
+		// Primary Candidate Direction
+		// c_1 = m1 * Vcf(c_0, kt) + m2 * Vcf(c_0, 
+		// Where kt = toGoal.heading/angRes
+		
+		float kt = toGoal.angle()/angRes; // kt
+		float tn = myPose.getHeading(); // theta n, current orientation
+
+		return m1 * DcCost(c0, kt) + m2 * DcCost(c0, tn/angRes) + m3 * DcCost(c0, lastPose.getHeading());
+	}
+	
+	public float DcCost(float c1, float c2) {
+		// Delta C Cost
+		// returns min{ |c1 - c2|, |c1 - c2 - 360/a|, |c1 - c2 + 360/a| }
+		// where a is the angular resolution of the histogram
+		
+ 		float firstmin = Math.min(Math.abs(c1-c2), Math.abs(c1-c2-FULLROT/angRes));
+ 		return Math.min(firstmin, Math.abs(c1-c2+FULLROT/angRes));
+	}
 }
